dbc29077ed808ed84813b5340b1cf89c4d82422c
Asherah Connor <ashe@kivikakk.ee>
2024-08-31 17:52:39 +0300

    build VexRiscv for ava.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "VexRiscv build tools";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in rec {
+      formatter = pkgs.alejandra;
+
+      packages.default = pkgs.writeScriptBin "build" ''
+        ${pkgs.sbt}/bin/sbt "runMain ee.kivikakk.ava.GenAva"
+      '';
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.sbt
+          pkgs.verilator
+        ];
+      };
+    });
+}
---
+++ src/main/scala/ee/kivikakk/ava/GenAva.scala
@@ -0,0 +1,75 @@
+package ee.kivikakk.ava
+
+// Based on GenSmallAndProductiveICache.
+// Changes noted.
+
+import vexriscv.plugin._
+import vexriscv.{VexRiscv, VexRiscvConfig, plugin}
+import spinal.core._
+import vexriscv.ip.InstructionCacheConfig
+
+/**
+ * Created by spinalvm on 15.06.17.
+ */
+object GenAva extends App{
+  def cpu() = new VexRiscv(
+    config = VexRiscvConfig(
+      plugins = List(
+        new IBusCachedPlugin(
+          resetVector = 0x00000000l,            // <-
+          compressedGen = true,                 // <-
+          injectorStage = true,                 // <- for compressedGen (ANTICIPATE)
+          config = InstructionCacheConfig(
+            cacheSize = 4096,
+            bytePerLine = 32,
+            wayCount = 1,
+            addressWidth = 32,
+            cpuDataWidth = 32,
+            memDataWidth = 32,
+            catchIllegalAccess = false,
+            catchAccessFault = false,
+            asyncTagMemory = false,
+            twoCycleRam = false,
+            twoCycleCache = true
+          )
+        ),
+        new DBusSimplePlugin(
+          catchAddressMisaligned = false,
+          catchAccessFault = false
+        ),
+        new CsrPlugin(CsrPluginConfig.smallest),
+        new DecoderSimplePlugin(
+          catchIllegalInstruction = false
+        ),
+        new RegFilePlugin(
+          regFileReadyKind = plugin.SYNC,
+          zeroBoot = false
+        ),
+        new IntAluPlugin,
+        new SrcPlugin(
+          separatedAddSub = false,
+          executeInsertion = true
+        ),
+        new LightShifterPlugin,
+        new HazardSimplePlugin(
+          bypassExecute           = true,
+          bypassMemory            = true,
+          bypassWriteBack         = true,
+          bypassWriteBackBuffer   = true,
+          pessimisticUseSrc       = false,
+          pessimisticWriteRegFile = false,
+          pessimisticAddressMatch = false
+        ),
+        new MulPlugin,                          // <- don't forget to infer DSP slices!
+        new DivPlugin,                          // <- for all of "M"
+        new BranchPlugin(
+          earlyBranch = false,
+          catchAddressMisaligned = false
+        ),
+        new YamlPlugin("cpu0.yaml")
+      )
+    )
+  )
+
+  SpinalVerilog(cpu())
+}
